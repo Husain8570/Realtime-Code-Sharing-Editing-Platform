07db4cc55ff6f00efc209e0348087caaa548c57d
Husain8570 <164224195+Husain8570@users.noreply.github.com>
2025-03-26 19:50:55 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <Box sx={{ position: "fixed", bottom: "0", width: "100vw" }}>
       <Typography sx={{ textAlign: "center", color: "#00E5B5" }}>
-        Nitish Gupta
+        Husain Matkawala
       </Typography>
     </Box>
   );
